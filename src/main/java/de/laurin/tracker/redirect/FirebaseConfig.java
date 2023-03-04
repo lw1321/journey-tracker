94b60978bd93f1a16b98d60eb2093142cf05b65b
laurin wandel <laurin.wandel@googlemail.com>
2023-03-04 18:59:57 +0100

    ,,

--- src/main/java/de/laurin/tracker/redirect/FirebaseConfig.java
+++ src/main/java/de/laurin/tracker/redirect/FirebaseConfig.java
@@ -3,16 +3,12 @@ package de.laurin.tracker.redirect;
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
-import lombok.Getter;
-import lombok.Setter;
-import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import java.io.ByteArrayInputStream;
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 
