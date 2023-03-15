4b0500f061d11e5fc5dd27001e2afed41680f091
Jonas Bark <jonas.bark@gmx.de>
2023-03-15 14:41:01 +0100

    store map bounds, google maps link, fix comment display

--- src/main/resources/static/index.js
+++ src/main/resources/static/index.js
@@ -1,4 +1,17 @@
-const map = L.map('map').setView([49.505, 10], 5);
+const map = L.map('map');
+
+let mapBounds = window.localStorage.getItem('mapBounds');
+if (mapBounds) {
+    [west, south, east, north] = mapBounds.split(',').map(parseFloat)
+    let bounds = new L.LatLngBounds(new L.LatLng(south, west), new L.LatLng(north, east))
+    map.fitBounds(bounds);
+} else {
+    map.setView([49.505, 10], 5);
+}
+map.on('moveend', function(e) {
+    let bounds = map.getBounds();
+    window.localStorage.setItem('mapBounds', bounds.toBBoxString());
+});
 
 let imageWidth = '100%'; // set the width of the image popup
 let imageHeight = '100%'; // set the height of the image popup
@@ -24,7 +37,9 @@ fetch('https://tellmemore.dev/v1/locations')
             L.marker([location.latitude, location.longitude], {
                 icon: bikeIcon,
                 zIndexOffset: 1000,
-            }).addTo(map);
+            }).addTo(map).on('click', function(e) {
+                window.open('https://www.google.de/maps/place/' + location.latitude + '+' + location.longitude + '/@' + location.latitude + '+' + location.longitude + ',15z', '_blank');
+            });
         }
     })
     .catch(error => {
@@ -70,19 +85,28 @@ fetch('https://tellmemore.dev/v1/location-images')
 
             let imageIcon = L.icon({
                 iconUrl: imageLocation.thumbUrl,
-                iconSize: [30, 30], // size of the icon
+                iconSize: [40, 40], // size of the icon
                 iconAnchor: [25, 25], // point of the icon which will correspond to marker's location
-                popupAnchor: [-3, -76] // point from which the popup should open relative to the iconAnchor
+                popupAnchor: [-3, -12] // point from which the popup should open relative to the iconAnchor
             });
             let marker = L.marker([imageLocation.latitude, imageLocation.longitude], {
                 icon: imageIcon
             }).addTo(map);
             let imageDate = new Date(imageLocation.createdDate * 1000);
-            let comment = (imageLocation.comment == "null" || "0") ? "" : imageLocation.comment;
+            let comment = (imageLocation.comment === "null" || imageLocation.comment === "0") ? "" : imageLocation.comment;
             let formattedDate = imageDate.toLocaleDateString('de-DE'); //imageDate.toLocaleTimeString('de-DE'); // Format as string
-            let popupContent = '<div><img src="' + imageLocation.thumbUrl + '" width="' + imageWidth + '" height="' + imageHeight + '"></div><div>' + formattedDate + "   " + comment + '</div>';
+            let popupContent = '<div>' +
+                    '<a href="' + imageLocation.imageUrl + '" target="_blank">' +
+                     '<img src="' + imageLocation.thumbUrl + '" width="' + imageWidth + '" height="' + imageHeight + '"></div>' +
+                    '</a>' +
+                    '<div>' + formattedDate + " </br>  " + comment + "</div>" +
+                    '<div><a href="https://www.google.de/maps/place/' + imageLocation.latitude + '+' + imageLocation.longitude + '/@' + imageLocation.latitude + '+' + imageLocation.longitude + ',15z" target="_blank">Open in Google Maps</a></div>' +
+                '</div>';
 
-            marker.bindPopup(popupContent).openPopup();
+            let popup = marker.bindPopup(popupContent);
+            if (i === data.length -1) {
+                popup.openPopup();
+            }
         }
 
     })
