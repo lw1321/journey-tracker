c752b4d87929177f10f5e54ead0ac127a2038293
laurin wandel <laurin.wandel@googlemail.com>
2023-05-02 15:24:12 +0200

    Update index.js

--- src/main/resources/static/index.js
+++ src/main/resources/static/index.js
@@ -25,7 +25,7 @@ let bikeIcon = L.icon({
 });
 
 
-tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
